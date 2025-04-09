e3b1e1dfb3e8407a1919be84babce6358b52608f
Danilo Machuca de Souza <danilo.machuca111@gmail.com>
2025-04-09 09:36:57 -0300

    modulo ajax completo

--- Ajax/main.js
+++ Ajax/main.js
@@ -27,19 +27,52 @@ $(document).ready(function () {
     $(btn).find("span").removeClass("d-none");
 
     //Adiquiri os dados na api
-    $.ajax(endPoint).done(function (resposta) {
-      const logradouro = resposta.logradouro;
-      const bairro = resposta.bairro;
-      const cidade = resposta.localidade;
-      const estado = resposta.uf;
+    // $.ajax(endPoint).done(function (resposta) {
+    //   const logradouro = resposta.logradouro;
+    //   const bairro = resposta.bairro;
+    //   const cidade = resposta.localidade;
+    //   const estado = resposta.uf;
 
-      const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+    //   const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
 
-      //Coloca os Dados na tela
-      $("#endereco").val(endereco);
+    //   //Coloca os Dados na tela
+    //   $("#endereco").val(endereco);
 
-      $(btn).find("span").addClass("d-none");
-      $(btn).find("i").removeClass("d-none");
-    });
+    //   $(btn).find("span").addClass("d-none");
+    //   $(btn).find("i").removeClass("d-none");
+    // });
+
+    fetch(endPoint)
+      .then(function (resposta) {
+        return resposta.json();
+      })
+      .then(function (json) {
+        const logradouro = json.logradouro;
+        const bairro = json.bairro;
+        const cidade = json.localidade;
+        const estado = json.uf;
+
+        const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+
+        $("#endereco").val(endereco);
+
+        $(btn).find("span").addClass("d-none");
+        $(btn).find("i").removeClass("d-none");
+      })
+      .catch(function (error) {
+        alert("Ocorreu um erro ao buscar o cep");
+      })
+      .finally(function () {
+        $(btn).find("span").addClass("d-none");
+        $(btn).find("i").removeClass("d-none");
+      });
+  });
+
+  $("#formulario-pedido").submit(function (e) {
+    e.preventDefault();
+
+    if ($("#nome").val().length == 0) {
+      throw new Error("Digite o nome");
+    }
   });
 });

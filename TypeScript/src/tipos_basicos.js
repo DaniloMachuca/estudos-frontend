bc9a8b7a2ec68a5d3d8143fdc52a6ddbc8192ab7
Danilo Machuca de Souza <danilo.machuca111@gmail.com>
2025-04-22 15:17:30 -0300

    começo do estudo de typescript

---
+++ TypeScript/src/tipos_basicos.js
@@ -0,0 +1,25 @@
+"use strict";
+//variaveis
+let estaChovendo = true;
+let idade = 37;
+let altura = 1.83;
+const nacionalidade = "Brasileira";
+//arrays
+const colegas = ["joao", "maria", "pedro"];
+const tecnologia = ["html", "css", "js"];
+const notas = [7, 8, 9, 10];
+//tuplas
+const lista = [
+    "joao",
+    true,
+    37,
+];
+//unions
+let idadeDaAna = 25;
+idadeDaAna = "25";
+//any
+let dadosDaAPI;
+dadosDaAPI = 10;
+dadosDaAPI = "string";
+dadosDaAPI = true;
+dadosDaAPI = [10, 20, 30];
---
+++ TypeScript/src/tipos_basicos.ts
@@ -0,0 +1,31 @@
+//variaveis
+let estaChovendo: boolean = true;
+
+let idade: number = 37;
+
+let altura: number = 1.83;
+
+const nacionalidade: string = "Brasileira";
+
+//arrays
+const colegas: string[] = ["joao", "maria", "pedro"];
+const tecnologia: Array<string> = ["html", "css", "js"];
+const notas: ReadonlyArray<number> = [7, 8, 9, 10];
+
+//tuplas
+const lista: [nome: string, estaEstudando: boolean, idade: number] = [
+  "joao",
+  true,
+  37,
+];
+
+//unions
+let idadeDaAna: number | string = 25;
+idadeDaAna = "25";
+
+//any
+let dadosDaAPI: any;
+dadosDaAPI = 10;
+dadosDaAPI = "string";
+dadosDaAPI = true;
+dadosDaAPI = [10, 20, 30];

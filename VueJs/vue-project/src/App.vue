4c0a29e358adf105bad83c36d5d16596a5d5e4bd
Danilo Machuca de Souza <danilo.machuca111@gmail.com>
2025-05-19 09:49:05 -0300

    a

--- VueJs/vue-project/src/App.vue
+++ VueJs/vue-project/src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { reactive } from 'vue'
+
 const nome = 'gian'
 const meuObj = {
   nome: 'gian',
@@ -19,6 +21,34 @@ const botaoEstaDesabilitado = true
 
 const gostaDoLink = false
 const gostaDaZelda = false
+
+const estado = reactive({
+  contador: 0,
+  email: '',
+  saldo: 5000,
+  transferindo: 0,
+})
+
+function incrementar() {
+  estado.contador++
+}
+function decrementar() {
+  estado.contador--
+}
+
+function alteraEmail(event) {
+  estado.email = event.target.value
+}
+
+function mostraSaldoFuturo() {
+  const { saldo, transferindo } = estado
+  return saldo - transferindo
+}
+
+function validarTransferencia() {
+  const { saldo, transferindo } = estado
+  return saldo >= transferindo
+}
 </script>
 
 <template>
@@ -28,10 +58,48 @@ const gostaDaZelda = false
   <h2 v-else>Não curte the legend of zelda</h2>
 
   <button :disabled="botaoEstaDesabilitado">enviar msg</button>
+
+  <br />
+  <hr />
+
+  {{ estado.contador }}
+
+  <button @click="incrementar" type="button">+</button>
+  <button @click="decrementar" type="button">-</button>
+
+  <br />
+  <hr />
+
+  {{ estado.email }}
+
+  <input type="email" @keyup="alteraEmail" />
+
+  <br />
+  <hr />
+
+  saldo: {{ estado.saldo }}
+  <br />
+  transferindo: {{ estado.transferindo }}
+  <br />
+  saldo depois da transferencia:{{ mostraSaldoFuturo() }}
+  <br />
+  <input
+    :class="{ invalido: !validarTransferencia() }"
+    @keyup="estado.transferindo = $event.target.value"
+    type="number"
+    placeholder="quantia para transferir"
+  />
+  <br />
+  <button v-if="validarTransferencia()">Transferir</button>
+  <span v-else>Valor insuficiente</span>
 </template>
 
 <style scoped>
 img {
   width: 400px;
+}
+
+.invalido {
+  outline-color: red;
 }
 </style>
